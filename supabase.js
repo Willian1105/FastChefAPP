157386d4d6b96466bab0d1e8c0532be43ac6ddd4
devdbarba <jonaspereiracps1@gmail.com>
2025-06-18 17:04:37 -0300

    adiciona script build para deploy na Vercel

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'SUPABASE_URL'; // substitua pela sua URL
-const SUPABASE_ANON_KEY = 'SUPABASE_ANON_KEY';               // substitua pela sua chave anon
+const SUPABASE_URL = 'https://cziiakrkhjsjmtiiocxm.supabase.co'; // substitua pela sua URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6aWlha3JraGpzam10aWlvY3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2NzU0MTEsImV4cCI6MjA2MzI1MTQxMX0.jwvLzm25vXjL3Pyrk5QV-tPKzm_VEuKnSMn-3MGqatI';;               // substitua pela sua chave anon
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
